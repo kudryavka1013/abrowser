d8751c374cf1a604f1c3a54b2f741476884646ee
kudryavka1013 <50348668+kudryavka1013@users.noreply.github.com>
2021-05-31 18:42:17 +0800

    Initial commit

---
+++ abrowser/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  abrowser
+//
+//  Created by 李兴利 on 2021/5/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ abrowser/abrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  abrowserApp.swift
+//  abrowser
+//
+//  Created by 李兴利 on 2021/5/31.
+//
+
+import SwiftUI
+
+@main
+struct abrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
