7cab2aee4018d0d5910904b3878436c5086bb9e5
fff824 <964048944@qq.com>
2021-06-11 18:51:56 +0800

    1

--- abrowser/HistoryAndBookmark/BookmarkCellView.swift
+++ abrowser/HistoryAndBookmark/BookmarkCellView.swift
@@ -7,20 +7,16 @@
 
 import SwiftUI
 
-struct BookmarkCellView : View{
-    var body: some View{
-        Text("bookmark cell view")
+/* struct BookmarkCellView: View {
+    var model: BookmarkModel
+    var body: some View {
+        HStack{ VStack(alignment: .leading) { Text(model.title); Text(model.subTitle) }; Spacer() }.padding()
     }
 }
-//struct BookmarkCellView: View {
-//    var model: BookmarkModel
-//    var body: some View {
-//        HStack{ VStack(alignment: .leading) { Text(model.title); Text(model.subTitle) }; Spacer() }.padding()
-//    }
-//}
-//
-//struct BookmarkCellView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        BookmarkCellView(model: BookmarkCellView(model:BookmarkModel(title: "Item Title", subTitle: "Item SubTitle")).previewLayout(.fixed(width: 375, height: 60)) as! BookmarkModel
-//        ) }
-//}
+
+struct BookmarkCellView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookmarkCellView(model: BookmarkCellView(model:BookmarkModel(title: "Item Title", subTitle: "Item SubTitle")).previewLayout(.fixed(width: 375, height: 60)) as! BookmarkModel
+        ) }
+}
+*/
--- abrowser/HistoryAndBookmark/BookmarkView.swift
+++ abrowser/HistoryAndBookmark/BookmarkView.swift
@@ -7,56 +7,83 @@
 
 import SwiftUI
 
-struct BookmarkView: View{
-    var body: some View{
-        Text("bookmark view")
+struct DestinationPageView: View {
+    var color: Color
+    var body: some View {
+        Text("Destination Page")
+            .font(.title)
+            .foregroundColor(color)
+    }
+}
+struct Bookmark:Identifiable, Hashable {
+    var id = UUID()
+    var name = ""
+    var children: [Bookmark]? = nil
+    var description: String {
+        switch children {
+        case nil:
+            return "🔖 \(name)"
+        case .some(let children):
+            return children.isEmpty ? "📂 \(name)" : "📁 \(name)"
+        }
     }
 }
 
-//struct Bookmark: Identifiable,Hashable {
-//    var id = UUID()
-//    var name: String
-//}
-//
-//struct BookmarkRow: View {
-//    var bookmark: Bookmark
-//
-//    var body: some View {
-//        Text("\(bookmark.name)")
-//    }
-//}
-//
-//struct ListModel {
-//    var title: String;
-//    var subTitle: String;
-//
-//    init(title: String, subTitle: String) {
-//        self.title = title;
-//        self.subTitle = subTitle
-//    }
-//}
-//
-//struct BookmarkView: View {
-//    var dataSource = (0..<5).map({ListModel(title: "Item \($0)", subTitle: "Sub \($0)")})
-//
-//        var body: some View {
-//        List(dataSource.identified(by: \.title)) { model in BookmarkCellView(model: model) }.padding()
-//
-///*        func deleteRow(at offsets:IndexSet) {
-//            Bookmark.remove(atOffsets:offsets)
-//                }
-//
-//                return List{
-//                    ForEach(Bookmark, id: \.self) { Bookmark in
-//                        BookmarkRow(bookmark: Bookmark)
-//                    }.onDelete(perform: deleteRow)
-//                } */
-//    }
-//}
+struct BookmarkView: View {
+    //    @ObservedObject var navigationState : NavigationState
+    
+    var item = [
+        Bookmark(name: "文件夹1",children: [
+            Bookmark(name: "书签1",children: nil),
+            Bookmark(name: "书签2",children: nil),
+            Bookmark(name: "书签3",children: nil)
+        ]),
+        Bookmark(name: "文件夹2",children: [
+            Bookmark(name: "文件夹3",children: [
+                Bookmark(name: "书签7",children: nil),
+                Bookmark(name: "书签8",children: nil),
+                Bookmark(name: "书签9",children: nil)
+            ]),
+            Bookmark(name: "书签4",children: nil),
+            Bookmark(name: "文件夹4",children: [])
+        ]),
+        Bookmark(name: "书签5",children: nil)
+    ]
+    
+    var body: some View {
+        
+        NavigationView{
+            BookmarkCellView(data: item)
+        }
+    }
+}
+
+struct BookmarkCellView : View{
+    var data : [Bookmark]
+    
+    func deleteRow (at offsets:IndexSet){
+        print("删除");
+    }
+    
+    var body: some View {
+        
+        List{
+            ForEach(data){ item in
+                if(item.children != nil){
+                    NavigationLink(destination: BookmarkCellView(data: item.children!)){
+                        Text(item.description)
+                    }
+                }
+                else{
+                    Text(item.description)
+                }
+            }.onDelete(perform: deleteRow)
+        }
+    }
+}
 
 struct BookmarkView_Previews: PreviewProvider {
     static var previews: some View {
         BookmarkView()
     }
 }
-
