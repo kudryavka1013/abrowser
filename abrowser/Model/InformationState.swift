459d0d7699e75ff6dd64e04bdbb484842a1d9fc0
kudryavka1013 <50348668+kudryavka1013@users.noreply.github.com>
2021-07-07 10:19:59 +0800

    Update InformationState.swift

--- abrowser/Model/InformationState.swift
+++ abrowser/Model/InformationState.swift
@@ -77,12 +77,12 @@ class InformationState: ObservableObject {
         task.resume()
     }
     
-    func getDictionaryFromJSONString(jsonString:String) ->NSDictionary{
-        let jsonData:Data = jsonString.data(using: .utf8)!
-        let dict = try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)
-        if dict != nil {
-            return dict as! NSDictionary
-        }
-        return NSDictionary()
-    }
+//    func getDictionaryFromJSONString(jsonString:String) ->NSDictionary{
+//        let jsonData:Data = jsonString.data(using: .utf8)!
+//        let dict = try? JSONSerialization.jsonObject(with: jsonData, options: .mutableContainers)
+//        if dict != nil {
+//            return dict as! NSDictionary
+//        }
+//        return NSDictionary()
+//    }
 }
